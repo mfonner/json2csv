2403264fe197f3296fbaa0aa77fa9778a172b9d3
Matt Fonner <matt@matthewfonner.com>
2020-03-03 21:09:42 -0500

    Added argument checking/validation as well as input file reads.

--- json2csv.py
+++ json2csv.py
@@ -3,10 +3,56 @@
 import sys
 import json
 import csv
+import argparse
+import os
+
+# Gathering args
+parser = argparse.ArgumentParser(
+        description='A script to convert a given json file to csv.')
+
+parser.add_argument('-i', 
+        help='json file to be converted. Note this will not overwrite the given file.')
+
+parser.add_argument('-o',
+        help='Output file to write as a csv.')
+
+args = parser.parse_args()
+
+
+def json_print(obj):
+    
+    # Prints json neatly
+    print(json.dumps(obj, indent=4, sort_keys=True))
+
 
 def main():
 
-    # TODO: This.
+    # Checking if the supplied json file exists
+    if os.path.exists(args.i):
+        
+        with open(args.i, 'r') as read_file:
+            data = json.load(read_file)
+
+            # Debugging print statement
+            json_print(data)
+
+            # TODO: Filter through the data for:
+            '''
+	    "login": {
+        	"uris": [
+          	{
+            	"match": null,
+            	"uri": "URL HERE"
+          	}
+        	],
+            "username": "VALUE",
+            "password": "VALUE"
+            '''
+
+    else:
+        print(f'Supplied input file {args.i} does not exist or we might not have permission to see it. Please try again.')
+        sys.exit(1)
+
 
 if __name__ == '__main__':
 
