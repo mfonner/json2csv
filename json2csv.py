bc6c32de52cea1784f74f0bcf99771ff6ab9c589
Matt Fonner <matt@matthewfonner.com>
2020-03-03 20:28:35 -0500

    Initial commit.

---
+++ json2csv.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+import csv
+
+def main():
+
+    # TODO: This.
+
+if __name__ == '__main__':
+
+    # Handle ^C without throwing an exception
+    try:
+        main()
+    except KeyboardInterrupt:
+        raise SystemExit
+        
